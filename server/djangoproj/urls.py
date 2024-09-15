3fde2e0f6ae09b71257bdf5d93741fec40f2fa07
vkul1234 <vedant.kulkarni242003@gmail.com>
2024-09-15 11:44:57 -0400

    Adding temporary changes to Github

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+        path('about/', TemplateView.as_view(template_name="About.html")),
+            path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
