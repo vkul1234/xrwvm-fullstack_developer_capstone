518052bdbac0b6421a7485f38024fa1c197db16c
vkul1234 <vedant.kulkarni242003@gmail.com>
2024-09-16 11:25:49 -0400

    changes made from theia lab environment

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
             path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
