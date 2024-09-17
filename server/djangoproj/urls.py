3465d2558639f2132c383425b2c50e434be2e290
vkul1234 <vedant.kulkarni242003@gmail.com>
2024-09-17 20:21:16 +0530

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,7 +1,6 @@
-"""
-djangoproj URL Configuration
+"""djangoproj URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
@@ -21,38 +20,14 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path(
-        'about/',
-        TemplateView.as_view(template_name="About.html")
-    ),
-    path(
-        'contact/',
-        TemplateView.as_view(template_name="Contact.html")
-    ),
+        path('about/', TemplateView.as_view(template_name="About.html")),
+            path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-    path(
-        'login/',
-        TemplateView.as_view(template_name="index.html")
-    ),
-    path(
-        'register/',
-        TemplateView.as_view(template_name="index.html")
-    ),
-    path(
-        'dealer/<int:dealer_id>',
-        TemplateView.as_view(template_name="index.html")
-    ),
-    path(
-        'postreview/<int:dealer_id>',
-        TemplateView.as_view(template_name="index.html")
-    ),
-    path(
-        'dealers/',
-        TemplateView.as_view(template_name="index.html")
-    ),
-    path(
-        '',
-        TemplateView.as_view(template_name="Home.html")
-    ),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
