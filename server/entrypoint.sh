fb1c2c8021f25aa5cdf5ecb83dc8967b0986fbdc
vkul1234 <vedant.kulkarni242003@gmail.com>
2024-09-17 16:04:10 -0400

    final app

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Make migrations and migrate the database.
 echo "Making migrations and migrating the database. "
-python manage.py makemigrations --run-syncdb --noinput
-python manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --run-syncdb --noinput
 python manage.py collectstatic --noinput
 exec "$@"
