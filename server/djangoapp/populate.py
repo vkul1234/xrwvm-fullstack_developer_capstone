8b34778edc132c7beffed4e3c4ae7099e0b63a76
vkul1234 <vedant.kulkarni242003@gmail.com>
2024-09-17 17:09:52 +0530

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,5 +1,6 @@
 from .models import CarMake, CarModel
 
+
 def initiate():
     car_make_data = [
         {"name": "NISSAN", "description": "Great cars. Japanese technology"},
@@ -16,8 +17,7 @@ def initiate():
             description=data['description']
         ))
 
-    car_model_data = [
-        {"name": "Pathfinder", "type": "SUV", "year": 2023, 
+    car_model_data = [{"name": "Pathfinder", "type": "SUV", "year": 2023, 
          "car_make": car_make_instances[0]},
         {"name": "Qashqai", "type": "SUV", "year": 2023, 
          "car_make": car_make_instances[0]},
