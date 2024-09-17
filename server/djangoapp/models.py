a1944365666bd1b6800d5fcc45b4b800613dcae5
vkul1234 <vedant.kulkarni242003@gmail.com>
2024-09-17 17:11:16 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
