2cf3be08c097160e3c3a3f2cf03827ffd2691db8
vkul1234 <vedant.kulkarni242003@gmail.com>
2024-09-17 17:20:25 +0530

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -17,7 +17,8 @@ def initiate():
             description=data['description']
         ))
 
-    car_model_data = [{"name": "Pathfinder", "type": "SUV", "year": 2023, 
+    car_model_data = [
+        {"name": "Pathfinder", "type": "SUV", "year": 2023, 
          "car_make": car_make_instances[0]},
         {"name": "Qashqai", "type": "SUV", "year": 2023, 
          "car_make": car_make_instances[0]},
